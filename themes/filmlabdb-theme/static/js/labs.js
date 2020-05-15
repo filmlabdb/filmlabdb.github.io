a4b91a289818fa9eacb72317a95a448a87ab89c0
Sam Smith <sams96@mail.com>
2020-05-15 22:02:59 +0100

    fix text string in cards

--- themes/filmlabdb-theme/static/js/labs.js
+++ themes/filmlabdb-theme/static/js/labs.js
@@ -1,14 +1,37 @@
 var labs = document.getElementById('labs').tBodies[0];
+var head = document.getElementById('labs').tHead.rows;
 var inner = labs.innerHTML 
 
 function reponsiveLabs(mq) {
 	if (mq.matches) {
-		Array.from(labs.rows).map(row => row.innerHTML = Array.from(row.cells).map(cell => cell.innerHTML).join(', '));
+		Array.from(labs.rows).map(
+			function(row) {
+				var r = [];
+				r.push(row.cells[0].innerHTML + ":");
+				r.push(row.cells[1].innerHTML);
+				if (Array.from(row.cells).slice(2, 5).some(i => i.innerHTML == "true")) {
+					r.push(head[0].cells[2].innerHTML + ":")
+					for (var i = 2; i < 5; i++) {
+						if (row.cells[i].innerHTML == "true") {
+							r.push(head[1].cells[i].innerHTML)
+						}
+					}
+				}
+				if (Array.from(row.cells).slice(5, 8).some(i => i.innerHTML == "true")) {
+					r.push(head[0].cells[3].innerHTML + ":")
+					for (var i = 5; i < 8; i++) {
+						if (row.cells[i].innerHTML == "true") {
+							r.push(head[1].cells[i].innerHTML)
+						}
+					}
+				}
+				row.innerHTML = r.join(' ');
+			});
 	} else {
 		labs.innerHTML = inner
 	}
 }
 
-var mq = window.matchMedia("(max-width: 42em)");
+var mq = window.matchMedia("(max-width: 24em)");
 reponsiveLabs(mq);
 mq.addListener(reponsiveLabs);
