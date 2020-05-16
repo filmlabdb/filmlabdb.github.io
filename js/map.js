5e24866f95ed746de8458786dc4f8a9b1d16a5a0
sams96 <sams96@users.noreply.github.com>
2020-05-16 21:37:50 +0000

    deploy: 00faf5f89af4517ec2a98446b16e18ef3f107e8d

--- js/map.js
+++ js/map.js
@@ -1,7 +1,13 @@
 var map = document.querySelector('#mapid');
 var loc = JSON.parse(map.dataset.loc).map(x => parseFloat(x))
+var pageType = map.dataset.type
 
-var mymap = L.map('mapid').setView(loc, 13);
+var zoomLevel = 2
+if (pageType == "single") {
+	zoomLevel = 13
+}
+
+var mymap = L.map('mapid').setView(loc, zoomLevel);
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 	attribution: 'TODO: add attribution',
 	maxZoom: 18,
@@ -11,4 +17,22 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
 	accessToken: 'pk.eyJ1Ijoic2Ftczk2IiwiYSI6ImNrYTc4eDFvbTAwdGEzMG10eWk1d3Z2c3QifQ.fOFRXNsIZ7xBQ4VtkWvJyw'
 }).addTo(mymap);
 
-var marker = L.marker(loc).addTo(mymap);
+if (pageType == "single") {
+	L.marker(loc).addTo(mymap)
+		.bindPopup(map.dataset.name)
+} else if (pageType == "map") {
+	var markers = L.markerClusterGroup();
+	fetch("../index.json")
+		.then(response => response.json())
+		.then(json => json.labs.map(
+			function(lab) { 
+				if (lab.loc != null) {
+					var marker = L.marker(lab.loc);
+					marker.bindPopup(lab.name);
+					console.log(lab.name);
+					markers.addLayer(marker);
+				}
+			}));
+
+	mymap.addLayer(markers);
+}
