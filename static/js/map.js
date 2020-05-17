fe9baf25c8c745bdd0250c60094dbaff866be745
Sam Smith <sams96@mail.com>
2020-05-17 18:44:55 +0100

    Add map attribution & analytics

--- static/js/map.js
+++ static/js/map.js
@@ -8,8 +8,9 @@ if (pageType == "single") {
 }
 
 var mymap = L.map('mapid').setView(loc, zoomLevel);
+mymap.attributionControl.setPrefix('');
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-	attribution: 'TODO: add attribution',
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery &copy; <a href="https://mapbox.com/about/maps">Mapbox</a>',
 	maxZoom: 18,
 	id: 'mapbox/streets-v11',
 	tileSize: 512,
